c02a2c5a43ae9f4f91ba03787f7a24cc6b7bb8df
Grace Sutherland <rgs1690@gmail.com>
2022-05-24 20:47:02 -0500

    added firebase auth in backend

--- CakeItSo/Program.cs
+++ CakeItSo/Program.cs
@@ -1,4 +1,7 @@
 using CakeItSo.Repos;
+using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,7 +15,23 @@ builder.Services.AddTransient<IEventRepo, EventRepo>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+FirebaseApp.Create(new AppOptions()
+{
+    Credential = GoogleCredential.FromFile(builder.Configuration["fbCredPath"]),
+});
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.IncludeErrorDetails = true;
+    options.Authority = "https://securetoken.google.com/authpractice-fbf28"; //use your project name
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidIssuer = "https://securetoken.google.com/authpractice-fbf28", //use your project name
+        ValidateAudience = true,
+        ValidAudience = "authpractice-fbf28",  //use your project name
+        ValidateLifetime = true,
+    };
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
--- client_cakeitso/src/App.js
+++ client_cakeitso/src/App.js
@@ -1,9 +1,45 @@
-
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import checkUserCreatedInDB from "./auth/checkIfUserCreatedInDB";
 import './App.css';
 import NavBar from './components/NavBar';
 import Routing from './routes';
 
 function App() {
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    firebase.auth.onAuthStateChanged(async (response) => {
+        if (response) {
+            const userObj = {
+                uid: response.uid,
+                fullName: response.displayName,
+                profilePic: response.photoURL,
+                username: response.email.split('@')[0],
+                token: response.accessToken, //you can save the token in an object if you want
+            };
+            setUser(userObj);
+            
+            //you can also do this to save the token for later use
+            sessionStorage.setItem("token", response.accessToken);
+
+            checkUserCreatedInDB();
+
+        } else {
+            setUser(false);
+             
+            //don't forget to clear the token if using sessionStorage!
+            sessionStorage.removeItem("token");
+
+            navigate('/');
+        }
+    });
+}, []);
+
+
   return (
     <div>
     <NavBar/>
