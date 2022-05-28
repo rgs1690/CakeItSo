153e8b32def16f0f6ba4375ff75972fa48182977
Grace Sutherland <rgs1690@gmail.com>
2022-05-28 13:28:04 -0500

    updated database for my uid

--- client_cakeitso/src/App.js
+++ client_cakeitso/src/App.js
@@ -23,7 +23,7 @@ function App() {
             };
             setUser(userObj);
             sessionStorage.setItem("token", response.accessToken);
-
+            console.log(userObj.uid)
             checkUserCreatedInDB(response.accessToken);
             navigate('/')
         } else {
