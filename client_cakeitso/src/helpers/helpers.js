f395d90b5a8c97937cef47f9e687d135cf6c3572
Grace Sutherland <rgs1690@gmail.com>
2022-06-02 21:31:33 -0500

    start

--- client_cakeitso/src/helpers/helpers.js
+++ client_cakeitso/src/helpers/helpers.js
@@ -1,9 +1,22 @@
-
 import { getAuth } from "@firebase/auth";
+import { getCakesByUserId } from "../api/cakeData";
+import { getCustomersByUserId } from "../api/customerData";
+import { getEventsByUserId } from "../api/eventData";
 
-const auth = getAuth()
+const auth = getAuth();
 const getCurrentUsersUid = () => auth.currentUser?.uid;
+
+const totalNumOfOrders = (uid) => {
+  getCustomersByUserId(uid).then((array) => {
+    return array.length;
+  });
+};
+
+const totalMoneyEarned = (uid) => {
+    const events = getEventsByUserId(uid); 
+    
+}
+
 
 
 export default getCurrentUsersUid;
-
